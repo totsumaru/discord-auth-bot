a749629428fc1da13d58b017f5534bf56f20a097
totsuka <totsuka@totsukanoMacBook-Air.local>
2023-08-05 12:01:39 +0900

    エラーハンドリングを修正

--- src/api/guild/guild.go
+++ src/api/guild/guild.go
@@ -25,72 +25,79 @@ type resServer struct {
 // 自分が管理できるサーバーの一覧を取得します
 func MyGuilds(e *gin.Engine) {
 	e.GET("/api/guild", func(c *gin.Context) {
+		header := c.GetHeader(api.HeaderAuthorization)
+		discordToken := strings.TrimPrefix(header, "Bearer ")
+
+		if discordToken == "" {
+			c.JSON(http.StatusBadRequest, "トークンに値が設定されていません")
+			return
+		}
 
 		res := Res{
 			Servers: []resServer{},
 		}
 
-		err := func() error {
-			header := c.GetHeader(api.HeaderAuthorization)
-			discordToken := strings.TrimPrefix(header, "Bearer ")
+		allGuilds, err := getAllGuilds(discordToken)
+		if err != nil {
+			c.JSON(http.StatusTooManyRequests, "You are being rate limited.")
+			return
+		}
 
-			// tokenから参加しているDiscordの一覧を取得
-			const url = "https://discordapp.com/api/users/@me/guilds"
-			req, err := http.NewRequest("GET", url, nil)
-			if err != nil {
-				return errors.NewError("httpリクエストを作成できません", err)
-			}
+		const iconURLTmpl = "https://cdn.discordapp.com/icons/%s/%s.png"
 
-			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", discordToken))
+		// botが導入されているサーバーのみをレスポンスとして返します
+		s := discord.Session
+		guilds := s.State.Guilds
 
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				return errors.NewError("httpリクエストを実行できません", err)
-			}
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return errors.NewError("bodyを読み取れません", err)
-			}
-
-			type apiResServer struct {
-				ID       string `json:"id"`
-				Name     string `json:"name"`
-				IconHash string `json:"icon"`
-			}
-
-			var apiRes = make([]apiResServer, 0)
-			if err = json.Unmarshal(body, &apiRes); err != nil {
-				return errors.NewError("jsonを構造体に変換できません", err)
-			}
-
-			const iconURLTmpl = "https://cdn.discordapp.com/icons/%s/%s.png"
-
-			// botが導入されているサーバーのみをレスポンスとして返します
-			s := discord.Session
-			guilds := s.State.Guilds
-
-			for _, v := range apiRes {
-				for _, guild := range guilds {
-					if v.ID == guild.ID {
-						res.Servers = append(res.Servers, resServer{
-							ID:      v.ID,
-							Name:    v.Name,
-							IconURL: fmt.Sprintf(iconURLTmpl, v.ID, v.IconHash),
-						})
-					}
+		for _, v := range allGuilds {
+			for _, guild := range guilds {
+				if v.ID == guild.ID {
+					res.Servers = append(res.Servers, resServer{
+						ID:      v.ID,
+						Name:    v.Name,
+						IconURL: fmt.Sprintf(iconURLTmpl, v.ID, v.IconHash),
+					})
 				}
 			}
-
-			return nil
-		}()
-		if err != nil {
-			errors.SendDiscord(err)
-			c.JSON(http.StatusInternalServerError, "エラーが発生しました")
-			return
 		}
 
 		c.JSON(http.StatusOK, res)
 	})
+}
+
+type getAllGuildsRes struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	IconHash string `json:"icon"`
+}
+
+// 自分が所属しているDiscordサーバーを全て取得します
+func getAllGuilds(discordToken string) ([]getAllGuildsRes, error) {
+	res := make([]getAllGuildsRes, 0)
+
+	// tokenから参加しているDiscordの一覧を取得
+	const url = "https://discordapp.com/api/users/@me/guilds"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return res, errors.NewError("httpリクエストを作成できません", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", discordToken))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return res, errors.NewError("httpリクエストを実行できません", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return res, errors.NewError("bodyを読み取れません", err)
+	}
+
+	if err = json.Unmarshal(body, &res); err != nil {
+		return res, errors.NewError("jsonを構造体に変換できません", err)
+	}
+
+	return res, nil
 }
---
+++ src/shared/debug/print.go
@@ -0,0 +1,9 @@
+package debug
+
+import "fmt"
+
+func PrintStr(v string) {
+	fmt.Println("===================")
+	fmt.Println(v)
+	fmt.Println("===================")
+}
